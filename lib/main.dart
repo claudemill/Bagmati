cb3145701e7b21286e396d8e3088f8e0b43b5ff7
tbhattarai <tbhattarai@gmail.com>
2020-03-26 08:45:17 +0545

    Initial Data Retrieval pushed, Async issue during data retrieval and display is yet to be fixed, code simplified

--- lib/main.dart
+++ lib/main.dart
@@ -1,151 +1,171 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//hello suman, this is to test comment 
+//hello suman, this is to test comment
 import 'dart:convert';
-
+import 'package:after_layout/after_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 
-class CovidAppBar extends StatelessWidget {
+void main() => runApp(new TabsApp());
+
+class TabsApp extends StatefulWidget {
+  
+  @override
+  TabsAppState createState() => new TabsAppState();
+}
+
+class TabsAppState extends State<TabsApp> {
+  // class TabsAppState extends State<TabsApp>  with AfterLayoutMixin<TabsApp>{
+
+  static Map finalCovidDataMap = new Map();
+  @override
+  void initState() {
+    super.initState();
+      getCovidData();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: choices.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Covid-19 Tracker'),
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: choices.map((CovidData choice) {
-                return Tab(
-                  text: choice.title,
-                  icon: choice.icon,
-                );
-              }).toList(),
+   List<Widget> containers = [
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            color: Colors.white,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Text(
+                  //     "Cases: ${TabsAppState.finalCovidDataMap['globalDataMap']['cases']}"),
+                  // Text(
+                  //     "Deaths: ${TabsAppState.finalCovidDataMap['globalDataMap']['deaths']}"),
+                  // Text(
+                  //     "Recovered: ${TabsAppState.finalCovidDataMap['globalDataMap']['recovered']}"),
+                ],
+              ),
             ),
           ),
-          body: TabBarView(
-            children: choices.map((CovidData choice) {
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: ChoiceCard(
-                  
-                ),
-              );
-            }).toList(),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            color: Colors.white,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Text(
+                  //     "Active: ${TabsAppState.finalCovidDataMap['localDataMap']['active']}"),
+                  // Text(
+                  //     "Total Cases: ${TabsAppState.finalCovidDataMap['localDataMap']['cases']}"),
+                  // Text(
+                  //     "Cases Reported Today: ${TabsAppState.finalCovidDataMap['localDataMap']['todayCases']}"),
+                  // Text(
+                  //     "Recovered Cases: ${TabsAppState.finalCovidDataMap['localDataMap']['recovered']}"),
+                  // Text(
+                  //     "Critical Cases: ${TabsAppState.finalCovidDataMap['localDataMap']['critical']}"),
+                  // Text(
+                  //     "Total Deaths: ${TabsAppState.finalCovidDataMap['localDataMap']['deaths']}"),
+                  // Text(
+                  //     "Deaths Today: ${TabsAppState.finalCovidDataMap['localDataMap']['todayDeaths']}"),
+                  // Text(
+                  //     "Cases Per Million: ${TabsAppState.finalCovidDataMap['localDataMap']['casesPerOneMillion']}"),
+                  // Text(
+                  //     "Deaths Per Million: ${TabsAppState.finalCovidDataMap['localDataMap']['deathsPerOneMillion']}"),
+                ],
+              ),
+            ),
           ),
         ),
-      ),
-    );
-  }
-}
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Card(
+            color: Colors.white,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [],
+              ),
+            ),
+          ),
+        )
+      ];
 
-class CovidData {
-  CovidData({this.title, this.icon,this.cases,this.todayCases,
-  this.todayDeaths,this.recovered,this.active,this.critical,this.casesPerOneMillion,this.deathsPerOneMillion});
+      print("after getting the covid data===in build=====>>>");
+      print("this is the data in map====${finalCovidDataMap.toString()}");
 
-   String title;
-   dynamic icon;
-  final String cases;
-  final String todayCases;
-  final String todayDeaths;
-  final String recovered;
-  final String active;
-  final String critical;
-  final String casesPerOneMillion;
-  final String deathsPerOneMillion;
+      print("inside build----after cards--==========>>");
 
-  CovidData.fromJson(Map<String, dynamic> json)
-      : cases = json['cases'],
-        todayCases = json['todayCases'],
-        todayDeaths = json['todayDeaths'],
-        recovered = json['recovered'],
-        active = json['active'],
-        critical = json['critical'],
-        casesPerOneMillion = json['casesPerOneMillion'],
-        deathsPerOneMillion = json['deathsPerOneMillion']
-        ;
-
-  Map<String, dynamic> toJson() =>
-    {
-      'cases': cases,
-      'todayCases': todayCases,
-      'todayDeaths':todayDeaths,
-      'recovered':recovered,
-      'active':active,
-      'critical':critical,
-      'casesPerOneMillion':casesPerOneMillion,
-      'deathsPerOneMillion':deathsPerOneMillion
-    };
-
-}
-
- fetchCovidData() async {
-  final response = await http.get('https://corona.lmao.ninja/countries/nepal');
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return CovidData.fromJson(json.decode(response.body));
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load covid data');
-  }
-}
-
-
-   List<CovidData> choices =  [
-   CovidData(title: 'Global Data', icon: Icon(FontAwesomeIcons.globe)),
-   CovidData(
-     title: 'Nepal Data', 
-   icon:ImageIcon(NetworkImage("https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/np.png")),
-   ),
-   CovidData(title: 'Questions', icon: Icon(FontAwesomeIcons.question)),
- 
-   ];
-
-class ChoiceCard extends StatelessWidget {
-  const ChoiceCard({Key key, this.choice}) : super(key: key);
-
-  final CovidData choice;
-
-  @override
-  Widget build(BuildContext context) {
-    final TextStyle textStyle = Theme.of(context).textTheme.display1;
-    return Card(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
-           children:[
-             Text("Data Here!"),
-             //fetchCovidData(),
-            // Text(choice.title, style: textStyle),
-            // choice.icon
-          ],
-
+      print("this is the data in map====${finalCovidDataMap.toString()}");
+      return new MaterialApp(
+        home: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Covid-19 Monitor"),
+              bottom: TabBar(
+                isScrollable: true,
+                tabs: <Tab>[
+                  Tab(
+                    icon: Icon(FontAwesomeIcons.globe),
+                    text: "Global Data",
+                  ),
+                  Tab(
+                    icon: ImageIcon(NetworkImage(
+                        "https://raw.githubusercontent.com/NovelCOVID/API/master/assets/flags/np.png")),
+                    text: "Nepal's Data",
+                  ),
+                  Tab(
+                    icon: Icon(FontAwesomeIcons.question),
+                    text: "Questions",
+                  )
+                ],
+              ),
+            ),
+            body: TabBarView(children: containers),
+          ),
         ),
-      ),
-    );
+      );
   }
-}
 
- fetchNepalData() {
-  print("hello hello=========");
-  Future <http.Response> covidData = http.get('https://corona.lmao.ninja/countries/nepal');
-  print(covidData);
-  Map covidDataMap= jsonDecode(covidData.toString());
-  var data=CovidData.fromJson(covidDataMap);
-  print('Hello this is recovered data=====>, ${data.recovered}');
-  return data;
-}
+    getCovidData() async {
+    print("inside getFinalCovidData=======");
+    finalCovidDataMap = await fetchCovidData();
+  }
 
-void main() {
-  runApp(CovidAppBar());
-  print(fetchNepalData());
+  Future<Map> fetchCovidData() async {
+    Map dataMap = new Map();
+    print("hello hello hello===inside fetchCovidData===>>>");
+    http.Response localResponse =
+        await http.get('https://corona.lmao.ninja/countries/nepal');
+    http.Response globalResponse =
+        await http.get('https://corona.lmao.ninja/all');
+
+    if (localResponse.statusCode == 200 && globalResponse.statusCode == 200) {
+      Map localDataMap = json.decode(localResponse.body);
+      Map globalDataMap = json.decode(globalResponse.body);
+
+      debugPrint("localResponse=====>>" + localResponse.body.toString());
+      debugPrint("globalResponse======>>" + globalResponse.body.toString());
+      dataMap['localDataMap'] = localDataMap;
+      dataMap["globalDataMap"] = globalDataMap;
+      print("success to load covid data======>>>" + dataMap.toString());
+      // If the server  returns a 200 OK response,
+      // then parse the JSON.
+      return dataMap;
+    } else {
+      print("failed to load covid data======>>>");
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load covid data');
+    
+  
+    }
+}
 }
