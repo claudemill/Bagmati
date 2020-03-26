06106ba48098d0b424ace77ca435a72ef61a1788
tbhattarai <tbhattarai@gmail.com>
2020-03-26 14:04:28 +0545

    removed unwanted comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//hello suman, this is to test comment
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
