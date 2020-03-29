ad750d80a230583ef092ccca5ba3232790ed73fa
tbhattarai <tbhattarai@gmail.com>
2020-03-29 16:16:47 +0545

    webview pulled for symptom checker and who faq

--- lib/CheckSymptoms.dart
+++ lib/CheckSymptoms.dart
@@ -6,7 +6,8 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class CheckSymptoms extends StatelessWidget {
-  final Completer<WebViewController> _controller=  Completer<WebViewController>();
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -34,60 +35,74 @@ class CheckSymptoms extends StatelessWidget {
             ),
           ),
           body: Container(
-            color: Colors.cyan,
-            child: Scaffold(
-                          body: TabBarView(
-                            physics: NeverScrollableScrollPhysics(),
-                            children: [
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Card(
-                                // color: Colors.blue,
-                                child: SymptomForm(),
-                              ),
+              color: Colors.cyan,
+              child: Scaffold(
+                body: TabBarView(
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Card(
+                          // color: Colors.blue[100],
+                          child: Scaffold(
+                            body: WebView(
+                              initialUrl:
+                                  "https://claudemill.com/index.php/corona/",
+                              javascriptMode: JavascriptMode.unrestricted,
+                              // onWebViewCreated:(WebViewController webViewcontroller ){
+                              //   _controller.complete(webViewcontroller);
+                              // },
                             ),
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Card(
-                                color: Colors.white,
-                                
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Card(
-                                // color: Colors.blue[100],
-                                child: Scaffold(
-                                                                  body: WebView(
-                                    initialUrl: "https://www.who.int/news-room/q-a-detail/q-a-coronaviruses",
-                                    javascriptMode: JavascriptMode.unrestricted,
-                                    // onWebViewCreated:(WebViewController webViewcontroller ){
-                                    //   _controller.complete(webViewcontroller);
-                                    // },
-                                  ),
-                                ),
-                              ),
-                            )
-                          ]),
-                          floatingActionButton: FutureBuilder<WebViewController>(
-                            future: _controller.future,
-                            builder: (BuildContext context, AsyncSnapshot<WebViewController> controller){
-                                // if(controller.hasData){
-                                //   return FloatingActionButton(
-                                //     child:Icon(Icons.alarm),
-                                //     onPressed: (){
-                                //       controller.data.loadUrl("https://youtube.com");
-                                //     },
-                                    
-                                //   );
-                                // }
-                                return Container();
-                            },
-                                                      
                           ),
-            )
-          ),
-        )
-        );
+                        ),
+                      ),
+                      // Padding(
+                      //   padding: const EdgeInsets.all(16.0),
+                      //   child: Card(
+                      //     // color: Colors.blue,
+                      //     child: SymptomForm(),
+                      //   ),
+                      // ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Card(
+                          color: Colors.white,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Card(
+                          // color: Colors.blue[100],
+                          child: Scaffold(
+                            body: WebView(
+                              initialUrl:
+                                  "https://www.who.int/news-room/q-a-detail/q-a-coronaviruses",
+                              javascriptMode: JavascriptMode.unrestricted,
+                              // onWebViewCreated:(WebViewController webViewcontroller ){
+                              //   _controller.complete(webViewcontroller);
+                              // },
+                            ),
+                          ),
+                        ),
+                      )
+                    ]),
+                floatingActionButton: FutureBuilder<WebViewController>(
+                  future: _controller.future,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<WebViewController> controller) {
+                    // if(controller.hasData){
+                    //   return FloatingActionButton(
+                    //     child:Icon(Icons.alarm),
+                    //     onPressed: (){
+                    //       controller.data.loadUrl("https://youtube.com");
+                    //     },
+
+                    //   );
+                    // }
+                    return Container();
+                  },
+                ),
+              )),
+        ));
   }
 }
